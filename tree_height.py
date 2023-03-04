58ceb235f08a2fd710ff13def599c13a8fc3e85d
ArtaBelavska <124885266+ArtaBelavska@users.noreply.github.com>
2023-03-04 15:08:18 +0000

    04.03.2023

--- tree_height.py
+++ tree_height.py
@@ -1,27 +1,52 @@
-# python3
-
+# # python3
 import sys
 import threading
-import numpy
+#import numpy
 
 
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+    # izveido sarakstu lai saglabatu mezlu skaitu
+    heights = [0] * n
+    # Atrast saknes skaitu
+    root = parents.index(-1)
+    # aprekinat katra mezgla augstumu
+    def dfs(skaits):
+        # atgriezt mezgla uagstumu ja tas ir aprekinats
+        if heights[skaits] != 0:
+            return heights[skaits]
+        if skaits == root:  # ja ir sakne tad garums ir 1
+            heights[skaits] = 1
+        else: # aprekina vecaka garumu un pieliek 1
+            heights[skaits] = dfs(parents[skaits]) + 1
+        return heights[skaits]
+    #aprekina katra mezgla augstumu
+    for i in range(n):
+        dfs(i)
+    # atgriez maksimalo garumu
+    return max(heights)
 
 
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
+    text=input("Enter I or F: ")
+    if "I" in text:
+        n = int(input())
+        parents = list(map(int, input().split()))
+    elif "F" in text:
+        filename=input()
+        if "a" not in filename:
+            try:
+                with open ("/test/16", mode="r") as fails:
+                    text=fails.read()
+                    parents=list(map(int, fails.readline().split()))
+            except Exception as j:
+                print("Error:", str(j))
+                return
+        else:
+            print("Error: invalid filename")
+            return
+    max_height = compute_height(n, parents)
+    print(max_height)
+
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
@@ -29,5 +54,3 @@ def main():
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
